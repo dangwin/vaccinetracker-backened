4eb27f1daacbb0e2988122205f2e253d19a80c3a
dangwin <danghwin@gmail.com>
2021-04-07 14:20:55 -0500

    seeded data

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,9 +2,33 @@ class PatientsController < ApplicationController
 
     def index
         @patients = Patient.all
-        render json: @Patient
+        render json: @patients
     end
 
+    def create
+        @patient = Patient.create(patient_params)
+        render json: @patient
+    end
+
+    def show
+        @patient = Patient.find(params[:id])
+        render json: @patient
+    end
+
+    # def updated
+    #     @patient.update(patient_params)
+    #     if @patient.save
+    #         render json: @patient
+    #     else 
+    #         render json {}
+    # end 
+
+
+    private
+
+    def patient_params
+        params.require(:patient).permit(:name, :age, :gender, :vaccine)
+    end
 
 
 end
